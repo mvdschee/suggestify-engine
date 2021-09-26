e50e779c54d138b38aef4ef50e742082d421942f
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2021-09-26 23:13:45 +0700

    added options to cors

--- api/events.js
+++ api/events.js
@@ -118,7 +118,7 @@ const allowCors = (fn) => async (req, res) => {
 
 	if (config.ALLOWED_ORIGINS.indexOf(origin) > -1) res.setHeader('Access-Control-Allow-Origin', origin);
 	res.setHeader('Access-Control-Allow-Credentials', true);
-	res.setHeader('Access-Control-Allow-Methods', 'POST');
+	res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
 	res.setHeader(
 		'Access-Control-Allow-Headers',
 		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
